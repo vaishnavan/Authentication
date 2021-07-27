4587bd2e11487426a8542a5dbd74092c16b67e21
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-07-28 00:49:28 +0530

    connection setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+const app = express();
+//env config
+require("dotenv").config();
+
+//server connection
+app.listen((port), (req, res) => {
+    console.log(`server up and running on port ${port}`);
+});
+
+//DB connection
+mongoose.connect((process.env.MONGO_URL), {useNewUrlParser: true, useUnifiedTopology:true}, (err) => {
+    if(!err){
+        console.log("Database created successfully");
+    }
+});
